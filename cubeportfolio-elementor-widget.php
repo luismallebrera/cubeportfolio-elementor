a0556411883b2b7a040cb2a70fb728ff4465abeb
luismallebrera <38189359+luismallebrera@users.noreply.github.com>
2025-11-28 01:43:43 +0000

    Agregar control de margin para items de filtros - v3.3.0

--- cubeportfolio-elementor-widget.php
+++ cubeportfolio-elementor-widget.php
@@ -621,6 +621,14 @@ add_action('elementor/widgets/register', function($widgets_manager){
                         '{{WRAPPER}} .cbp-l-filters-button .cbp-filter-item' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
                     ],
                 ]);
+                $this->add_responsive_control('filters_margin', [
+                    'label'     => esc_html__('Margin', 'cubeportfolio-elementor-widget'),
+                    'type'      => \Elementor\Controls_Manager::DIMENSIONS,
+                    'size_units'=> ['px', '%', 'em'],
+                    'selectors' => [
+                        '{{WRAPPER}} .cbp-l-filters-button .cbp-filter-item' => 'margin: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+                    ],
+                ]);
                 $this->add_control('filters_hover_color', [
                     'label'     => esc_html__('Color texto (Hover)', 'cubeportfolio-elementor-widget'),
                     'type'      => \Elementor\Controls_Manager::COLOR,
